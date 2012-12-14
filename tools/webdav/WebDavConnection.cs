7758673864067cd46c14780d778070b60bd84dfc
Jonathan Pryor <jonpryor@vt.edu>
2012-12-13 23:22:15 -0500

    [webdav] Add `webdav` tool.
    `webdav` is an ftp-like command-line program which uses
    WebDavMethodBuilder/etc. It's to easily test and observe behavior.

---
+++ tools/webdav/WebDavConnection.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Cadenza.Net;
+
+namespace Cadenza.Tools.WebDav {
+
+	[AttributeUsage (AttributeTargets.Method, AllowMultiple=true)]
+	class HelpAttribute : Attribute {
+
+		public HelpAttribute (string helpText)
+		{
+			if (helpText == null)
+				throw new ArgumentNullException ("helpText");
+			HelpText = helpText;
+		}
+
+		public string HelpText {get; private set;}
+	}
+
+	class WebDavConnection
+	{
+		public WebDavMethodBuilder Builder {get; private set;}
+
+		public WebDavConnection ()
+		{
+			Builder = new WebDavMethodBuilder ();
+		}
+
+		public static Action<WebDavConnection, string> GetCommand (string command)
+		{
+			Action<WebDavConnection, string> action;
+			if (Commands.TryGetValue (command, out action))
+				return action;
+			return null;
+		}
+
+		static readonly Dictionary<string, Action<WebDavConnection, string>> Commands = new Dictionary<string, Action<WebDavConnection, string>>() {
+			{ "help",     ShowHelp },
+			{ "exit",     Exit },
+			{ "server",   Server },
+			{ "ls",       ListPath },
+		};
+
+		[Help ("List all commands")]
+		static void ShowHelp (WebDavConnection state, string args)
+		{
+			Console.WriteLine ("webdav commands--");
+			foreach (var name in Commands.Keys.OrderBy (k => k)) {
+				var d = Commands [name];
+				var h = (HelpAttribute[]) d.Method.GetCustomAttributes (typeof (HelpAttribute), false);
+
+				var prefix = "  " + name + ":";
+
+				if (h == null || h.Length == 0) {
+					Console.WriteLine (prefix);
+					continue;
+				}
+				if (prefix.Length > 10) {
+					Console.WriteLine (prefix);
+					prefix = new string (' ', 10);
+				} else {
+					prefix = prefix.PadRight (10);
+				}
+				foreach (var a in h)
+					foreach (var l in Mono.Options.StringCoda.WrappedLines (a.HelpText, 50, 48)) {
+						Console.WriteLine ("{0}{1}", prefix, l);
+						prefix = new string (' ', 12);
+					}
+			}
+		}
+
+		static void Exit (WebDavConnection state, string ignored)
+		{
+			Environment.Exit (0);
+		}
+
+		[Help ("SERVER")]
+		[Help ("Open a connection to the specified WebDAV server.")]
+		static void Server (WebDavConnection state, string server)
+		{
+			if (!string.IsNullOrEmpty (server))
+				state.Builder.Server = new Uri (server);
+			else
+			    Console.WriteLine ("Server: {0}", state.Builder.Server);
+		}
+
+		[Help ("PATH")]
+		[Help ("List files at PATH")]
+		static void ListPath (WebDavConnection state, string path)
+		{
+			var t = string.IsNullOrEmpty (path)
+				? state.Builder.CreatePropertyFindMethodAsync ()
+				: state.Builder.CreatePropertyFindMethodAsync (path);
+			if (t.IsFaulted) {
+				Console.Error.WriteLine ("webdav: {0}", t.Exception.Flatten ());
+				return;
+			}
+			Console.WriteLine ("Response: {0}", t.Result.Response);
+			foreach (var e in t.Result.GetEntries ()) {
+				if (e.Exception != null)
+					Console.Error.WriteLine ("webdav: {0}", e.Exception);
+				else
+					Console.WriteLine ("Name={0} Directory={1} Path={2} Result={3}", e.Name, e.Directory, e.Path, e.Response);
+			}
+		}
+	}
+}
+
---
+++ tools/webdav/webdav.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Net;
+using System.Collections.Generic;
+
+using Mono.Options;
+using Mono.Terminal;
+
+namespace Cadenza.Tools.WebDav
+{
+	class App
+	{
+		public static void Main (string[] args)
+		{
+			var c = new WebDavConnection ();
+
+			bool show_help = false;
+			bool show_version = false;
+			var o = new OptionSet {
+				"Usage: webdav [OPTIONS]",
+				"",
+				"Simple WebDav command-line client to excercise WebDavClient.",
+				"",
+				"Options:",
+				{ "server=",
+				  "Set name of WebDAV {SERVER} to connect to.",
+				  v => c.Builder.Server = new Uri (v) },
+				{ "user=",
+				  "Set {USERNAME} on WebDAV server to connect to.",
+				  v => (c.Builder.Credentials ?? (c.Builder.Credentials = new NetworkCredential ())).UserName = v },
+				{ "pass=",
+				  "Set {PASSWORD} on WebDAV server to connect to.",
+				  v => (c.Builder.Credentials ?? (c.Builder.Credentials = new NetworkCredential ())).Password = v },
+				{ "version",
+				  "Show version information and exit.",
+				  v => show_version = v != null },
+				{ "help|h|?",
+				  "Show this message and exit.",
+				  v => show_help = v != null },
+			};
+
+			try {
+				o.Parse (args);
+			} catch (Exception ex) {
+				Console.Error.WriteLine ("webdav: {0}", ex.Message);
+			}
+
+			if (show_version) {
+				Console.WriteLine ("webdav 0.1");
+				return;
+			}
+			if (show_help) {
+				o.WriteOptionDescriptions (Console.Out);
+				return;
+			}
+
+			LineEditor e = new LineEditor ("webdav");
+			string s;
+			
+			while ((s = e.Edit ("webdav> ", "")) != null) {
+				if ((s = s.Trim ()).Length == 0)
+					continue;
+
+				var p = s.IndexOf (' ');
+				var m = p < 0 ? s : s.Substring (0, p);
+				var a = WebDavConnection.GetCommand (m);
+				if (a == null) {
+					Console.Error.WriteLine ("webdav: Invalid command: {0}", s);
+					continue;
+				}
+				while (p > 0 && p < s.Length && char.IsWhiteSpace (s, p))
+					++p;
+				s = p >= 0 && p < s.Length ? s.Substring (p) : "";
+				a (c, s);
+			}
+		}
+	}
+}
